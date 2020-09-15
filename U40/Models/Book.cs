9f11f1961b5c7b1eb05931271677e2050df7dfe9
UWP <54581408+hieu2k1@users.noreply.github.com>
2020-09-15 12:36:08 +0700

    LAB07

---
+++ U40/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U40
+{
+    class Book
+    {
+        public int BookId { get; set; }
+        public String Title { get; set; }
+        public String Author { get; set; }
+        public String CoverImage { get; set; }
+    }
+}
---
+++ U40/Models/BookManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U40.Models
+{
+    class BookManager
+    {
+        public static List<Book> GetBooks()
+        {
+            var books = new List<Book>();
+
+            books.Add(new Book { BookId = 1, Title = "Vulpate", Author = "Futurum", CoverImage = "Assets/1.png" });
+            books.Add(new Book { BookId = 2, Title = "Mazim", Author = "Sequiter Que", CoverImage = "Assets/2.png" });
+            books.Add(new Book { BookId = 3, Title = "Elip", Author = "Tempor", CoverImage = "Assets/3.png" });
+            return books;
+        }
+    }
+}
