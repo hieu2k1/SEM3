137982caedbe1e9184a2ea6614ca4b2abf065037
UWP <54581408+hieu2k1@users.noreply.github.com>
2020-10-16 07:09:52 +0700

    LAB6

---
+++ ContosoCrafts/ContosoCrafts.WebSite/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoCrafts.WebSite.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            this.ProductService = productService;
+        }
+        public JsonFileProductService ProductService { get; }
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+    }
+}
